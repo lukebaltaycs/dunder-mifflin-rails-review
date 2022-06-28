3964e1d2b6e62f25687639ecf4adbf3f750f2389
Luke Baltay <luke.baltay@yale.edu>
2022-06-28 16:49:01 -0400

    initial commit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+
+    belongs_to :dog
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
+
 end
