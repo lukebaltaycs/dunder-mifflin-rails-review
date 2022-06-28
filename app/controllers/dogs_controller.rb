41ae56866253af5e5a3da1e99573f69375c13e10
Luke Baltay <luke.baltay@yale.edu>
2022-06-28 15:34:17 -0400

    commit tree 8d9635c6afb5854d2eeeabffb7b14a81a5f4a2f4

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,56 @@
 class DogsController < ApplicationController
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+        @dog.assign_attributes(dog_params)
+        if @dog.valid?
+            @dog.save
+            flash[:notice] = "Dog succesfully updated!"
+            redirect_to dog_path(@dog)
+        else
+            render :edit
+        end
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        if @dog.valid?
+            @dog.save
+            flash[:notice] = "Dog succesfully created!"
+            redirect_to dog_path(@dog)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+
+        redirect_to dogs_path
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,57 @@
 class EmployeesController < ApplicationController
+
+        def show
+            @employee = Employee.find(params[:id])
+        end
+    
+        def index
+            @employees = Employee.all
+        end
+    
+        def edit
+            @employee = Employee.find(params[:id])    
+        end
+    
+        def update
+            @employee = Employee.find(params[:id])
+            @employee.assign_attributes(employee_params)
+            if @employee.valid?
+                @employee.save
+                flash[:notice] = "Employee succesfully updated!"
+                redirect_to employee_path(@employee)
+            else
+                render :edit
+            end
+        end
+    
+        def new
+            @employee = Employee.new
+        end
+    
+        def create
+            @employee = Employee.new(employee_params)
+            if @employee.valid?
+                @employee.save
+                flash[:notice] = "Employee succesfully created!"
+                redirect_to employee_path(@employee)
+            else
+                render :new
+            end
+        end
+    
+        def destroy
+            @employee = Employee.find(params[:id])
+            @employee.destroy
+    
+            redirect_to employees_path
+        end
+    
+    
+        private
+    
+        def employee_params
+            params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url)
+        end
+    
+        
 end
